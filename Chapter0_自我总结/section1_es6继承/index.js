e9eb73b0220e2ee302f14607662f38a7e5352d44
张帆 <zhangfan@zhangfan.local>
2020-04-02 16:02:32 +0800

    feat: 新增装饰器

--- Chapter0_自我总结/section1_es6继承/index.js
+++ Chapter0_自我总结/section1_es6继承/index.js
@@ -16,13 +16,16 @@ class SubClass_1 extends SuperClass{
        super(superClassName);
     }
 }
-class SubClass_2 extends SuperClass{
+class SubClass_2 extends SuperClass{}
 
-}
-console.log('new SubClass_1(SuperClass_SubClass_1).names.push(babyGirl)', new SubClass_1('SuperClass_SubClass_1').names.push('babyGirl'));
-console.log('new SubClass_2()',new SubClass_2()); 
-console.log('new SubClass_2().names',new SubClass_2().names); //['zf','cp']
-console.log('static SuperClass.getName',SuperClass.getName()); //undefined
+
+const sub_1_ins = new SubClass_1('SuperClass_SubClass_1');
+const sub_2_ins = new SubClass_2();
+
+console.log('sub_1_ins.names.push', sub_1_ins.names.push('babyGirl'),sub_1_ins.getSuperClassName()); 
+console.log('sub_1_ins.names',sub_1_ins.names); //['zf','cp','babyGirl'];
+
+console.log('sub_2_ins.names',sub_2_ins.names); //['zf','cp'];
 
 
 
--- Chapter0_自我总结/section6_react-redux/index.js
+++ Chapter0_自我总结/section6_react-redux/index.js
@@ -1,75 +1,115 @@
 let React = {
-    Component:{},
-    createContext(){}
-}
+  Component: {},
+  createContext() {}
+};
 
-const mapStateToProps = (status) => {
-    return {
-        counter:status.counter,
+const mapStateToProps = status => {
+  return {
+    counter: status.counter
+  };
+};
+
+const mapDispatchToProps = (dispath, ownProps) => {
+  return {
+    increment: count => dispath({ type: INCREMENT, count }),
+    decrement: count => dispath({ type: DECREMENT, count })
+  };
+};
+
+let connect = (mapStateToProps, mapDispatchToProps) => WrappedComponent => {
+  return class Connect extends React.Component {
+    //通过context上下文 来获取 store
+    constructor(props) {
+      this.store = StoreContext;
+      super(props);
+      (this.state = mapStateToProps(this.store.getState())),
+        (this.dispatch = mapDispatchToProps(this.store.dispatch));
     }
-}
-
-const mapDispatchToProps = (dispath,ownProps) => {
-    return {
-        increment:(count)=>dispath({ type:INCREMENT,count}),
-        decrement:(count)=>dispath({ type:DECREMENT,count}),
+    componentDidMount() {
+      this.unSubscrible = this.store.subscrible(() => {
+        this.state({
+          ...mapStateToProps(this.store.getState())
+        });
+      });
     }
-}
-
-let connect = (mapStateToProps,mapDispatchToProps) => (WrappedComponent)=>{
-    return class Connect extends React.Component {
-        //通过context上下文 来获取 store 
-        constructor(props){
-            this.store = StoreContext;
-            super(props);
-            this.state = mapStateToProps(this.store.getState()),
-            this.dispatch = mapDispatchToProps(this.store.dispatch)
-        }
-        componentDidMount() {
-            this.unSubscrible = this.store.subscrible(()=>{
-                this.state({
-                    ...mapStateToProps(this.store.getState())
-                })
-            })   
-        }
-        render () {
-            return (
-                <WrappedComponent {...this.props} {...this.state} {...this.dispatch}>
-                </WrappedComponent>
-            )
-        }
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          {...this.state}
+          {...this.dispatch}
+        ></WrappedComponent>
+      );
     }
-}
-
+  };
+};
 
 let StoreContext = React.createContext();
-class Provider extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    static childContextTypes = {
-        store: PropTypes.shape({
-            subscribe: PropTypes.func.isRequired,
-            dispatch: PropTypes.func.isRequired,
-            getState: PropTypes.func.isRequired
-        }).isRequired
-    }
-    constructor(props) {
-        super(props);
-        this.store = props.store;
-    }
+class Provider extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  static childContextTypes = {
+    store: PropTypes.shape({
+      subscribe: PropTypes.func.isRequired,
+      dispatch: PropTypes.func.isRequired,
+      getState: PropTypes.func.isRequired
+    }).isRequired
+  };
+  constructor(props) {
+    super(props);
+    this.store = props.store;
+  }
 
-    render(){
-        return (
-            <StoreContext.Provider value={this.props.store}>
-                {this.props.children}
-            </StoreContext.Provider>
-        )
-    }
-    
+  render() {
+    return (
+      <StoreContext.Provider value={this.props.store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
 }
 
 export default {
-    connect,
-    Provider
+  connect,
+  Provider
+};
+
+/**
+ * 在实际应用中，一个组件可能被多个 HOC增强，我们使用的是被所有的 HOC增强后的组件
+ * 高阶组件实际上就是装饰器模式
+ * 假设现在我们有 logger， visible， style等多个 HOC，现在要同时增强一个 Input组件：
+ * logger(visible(style(Input)))
+ * 所以我们封装一个组合函数 以装饰器模式来增强组件 即套娃模式
+ */
+function compose(){
+    let arr = Array.prototype.slice.call(arguments);  //Array.from(arguments)
+    /**
+     *  let q = ()=> fun_0(fun_1(fun_2(fun_3(...args))))
+    * */
+    let q = arr.reduce((pre,curr,idx,arr)=>{
+        return function(...args){
+            pre(curr(...args))
+        }
+    })
+    return q;
 }
+
+function fun_0(some){
+    console.log(`${some}_0`);
+    return `${some}_0`
+}
+function fun_1(some){
+    console.log(`${some}_1`);
+    return `${some}_1`
+}
+function fun_2(some){
+    console.log(`${some}_2`);
+    return `${some}_2`
+}
+function fun_3(some){
+    console.log(`${some}_3`);
+    return `${some}_3`
+}
+compose(fun_0,fun_1,fun_2,fun_3)('zf') //zf_3 zf_3_2 zf_3_2_1 zf_3_2_1_0
+
