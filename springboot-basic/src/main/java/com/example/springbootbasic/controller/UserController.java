1c2d274c0e3b121fbf52125a50c9d1249bb87a29
fordpzhang <2861669535@qq.com>
2023-01-05 17:11:20 +0800

    feat: up

--- springboot-basic/src/main/java/com/example/springbootbasic/controller/UserController.java
+++ springboot-basic/src/main/java/com/example/springbootbasic/controller/UserController.java
@@ -2,17 +2,25 @@ package com.example.springbootbasic.controller;
 
 import com.example.springbootbasic.Service.UserService;
 import com.example.springbootbasic.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+  private Logger logger = LoggerFactory.getLogger(UserController.class);
+
   @Autowired
   private UserService userService;
 
   @RequestMapping("/get/{id}")
   public User GetUser(@PathVariable int id){
+    logger.debug("测试logger标签");
+    logger.info("测试logger标签");
+    logger.warn("测试logger标签");
+    logger.error("测试logger标签");
     return userService.QueryById(id);
   }
 
