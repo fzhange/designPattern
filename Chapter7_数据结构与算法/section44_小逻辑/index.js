29b7fdc09782e1344c7a1ea1e9ed93acab973637
fzhange <28616695353@qq.com>
2023-09-24 17:19:08 +0800

    feat: closure question

--- Chapter7_数据结构与算法/section44_小逻辑/index.js
+++ Chapter7_数据结构与算法/section44_小逻辑/index.js
@@ -34,6 +34,15 @@ function randomSort(arr) {
 let arr = [1, 2, 3, 4, 5, 6];
 console.log("randomSort", randomSort(arr));
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+
 /**
  * 4
  * 深度clone
