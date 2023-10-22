5a818d1c55aa4df0237d01fcee5ef3b9656d769b
fzhange <fzhange@trip.com>
2023-10-22 19:42:58 +0800

    feat: add vue demo pro

---
+++ vue-demo/src/components/ChildComponent.vue
@@ -0,0 +1,28 @@
+<!-- ChildComponent.vue -->
+<template>
+  <div>
+    <slot message="this is a message from child compoinent"></slot>
+    <div @click="doClick">child</div>
+    <div>
+      <h1>provider inject</h1>
+      <h3 @click="changeDataFromStuffComponents">{{this.fatherApp.checkout_info}}</h3>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+  inject: ['fatherApp'],
+  methods:{
+    doClick(){
+      this.$emit('child-click','DoClick');
+    },
+    changeDataFromStuffComponents(){
+      this.fatherApp.checkout_info = 'changeDataFromStuffComponents'
+    }
+  },
+}
+</script>
